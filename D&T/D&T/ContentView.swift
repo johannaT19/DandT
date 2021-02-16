aec050fad6988ab72c5e2f1e61aed84322ff58be
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-02-16 09:29:51 +0100

    Create files

---
+++ D&T/D&T/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  D&T
+//
+//  Created by Johanna Tarento on 16/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Dress & Try")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ D&T/D&T/D_TApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_TApp.swift
+//  D&T
+//
+//  Created by Johanna Tarento on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct D_TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
