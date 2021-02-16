fbed7ee9a32660367e495e7e98e8c4accaf362b0
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-02-16 16:02:37 +0100

    Start home

--- D&T/D&T/ContentView.swift
+++ D&T/D&T/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Dress & Try")
-            .padding()
+        VStack {
+            Spacer().frame(height: 50)
+            HStack(alignment: .top, spacing: 10) {
+                Button(action: {}) {
+                    Text("Menu")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, -1.0)
+                }
+                Spacer()
+                Text("Dress & Try")
+                    .font(.title)
+                    .fontWeight(.ultraLight)
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .padding(0.0)
+                Spacer()
+            }
+            Spacer().frame(height: 100)
+            Text("Coucou")
+            Spacer()
+        }
+       
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+
     }
 }
